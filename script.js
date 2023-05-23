1e96c0973d7dc61d885cd9af75d0f8b54c536494
H-Ramalan <talktoramalan@gmail.com>
2023-05-22 17:33:11 -0700

    Add menu styling

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
-const navOpen = document.querySelector(".ham");
-const navClose = document.querySelector(".navClose");
+"use-strict";
+
+const modalOpen = document.querySelector(".ham");
+const modalClose = document.querySelector(".nav-close");
+const modal = document.querySelector(".modal-view");
+
+const openModal = () => {
+  modal.style.display = "flex";
+  event.preventDefault();
+};
+const closeModal = () => {
+  modal.style.display = "none";
+  event.preventDefault();
+};
+modalOpen.addEventListener("click", openModal);
+modalClose.addEventListener("click", closeModal);
+
+// const closeModal = function () {
+//   modal.classList.add("hidden");
+// };
+
+// const openModal = function () {
+//   modal.classList.remove("hidden");
+// };
+
+// modalOpen.addEventListener("click", openModal);
+// console.log("clicked");
+// modalClose.addEventListener("click", closeModal);
