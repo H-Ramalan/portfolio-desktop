4a54fc1c243e0e98bd8fbc78fe96989334788ec0
H-Ramalan <talktoramalan@gmail.com>
2023-05-23 03:22:27 -0700

    Fix eslint errors

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-"use-strict";
+'use-strict';
 
-const modalOpen = document.querySelector(".ham");
-const modalClose = document.querySelector(".nav-close");
-const modal = document.querySelector(".modal-view");
-const modalList = document.querySelectorAll(".list-modal");
+const modalOpen = document.querySelector('.ham');
+const modalClose = document.querySelector('.nav-close');
+const modal = document.querySelector('.modal-view');
+const modalList = document.querySelectorAll('.list-modal');
 
 const openModal = (e) => {
-  modal.style.display = "flex";
+  modal.style.display = 'flex';
   e.preventDefault();
 };
 
 const closeModal = () => {
-  modal.style.display = "none";
+  modal.style.display = 'none';
 };
 
-modalOpen.addEventListener("click", openModal);
-modalClose.addEventListener("click", closeModal);
+modalOpen.addEventListener('click', openModal);
+modalClose.addEventListener('click', closeModal);
 
 for (let i = 0; i < modalList.length; i += 1) {
-  modalList[i].addEventListener("click", closeModal);
+  modalList[i].addEventListener('click', closeModal);
 }
