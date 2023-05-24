ab13e240d96c8a04dd110bf6e1d37a913e1f4753
H-Ramalan <talktoramalan@gmail.com>
2023-05-23 17:09:05 -0700

    modal pop-up styling

--- script.js
+++ script.js
@@ -1,22 +1,75 @@
-'use-strict';
+"use-strict";
 
-const modalOpen = document.querySelector('.ham');
-const modalClose = document.querySelector('.nav-close');
-const modal = document.querySelector('.modal-view');
-const modalList = document.querySelectorAll('.list-modal');
+const modalOpen = document.querySelector(".ham");
+const modalClose = document.querySelector(".nav-close");
+const modal = document.querySelector(".modal-view");
+const modalList = document.querySelectorAll(".list-modal");
 
 const openModal = (e) => {
-  modal.style.display = 'flex';
+  modal.style.display = "flex";
   e.preventDefault();
 };
 
 const closeModal = () => {
-  modal.style.display = 'none';
+  modal.style.display = "none";
 };
 
-modalOpen.addEventListener('click', openModal);
-modalClose.addEventListener('click', closeModal);
+modalOpen.addEventListener("click", openModal);
+modalClose.addEventListener("click", closeModal);
 
 for (let i = 0; i < modalList.length; i += 1) {
-  modalList[i].addEventListener('click', closeModal);
+  modalList[i].addEventListener("click", closeModal);
 }
+
+// Modal Popup
+
+const projects = [
+  {
+    id: 0,
+    backImg: "images/pj-1.svg",
+    projectName: "Professional Art Printing Data",
+    projectNote:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    projectStackUsed: ["HTML", "Bootstrap", "Ruby"],
+  },
+  {
+    id: 1,
+    backImg: "images/project2.svg",
+    projectName: "Data Dashboard Healthcare",
+    projectNote:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    projectStackUsed: ["HTML", "Bootstrap", "Ruby"],
+  },
+  {
+    id: 2,
+    backImg: "images/pj3.png",
+    projectName: "Website Portfolio",
+    projectNote:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    projectStackUsed: ["HTML", "Bootstrap", "Ruby"],
+  },
+  {
+    id: 3,
+    backImg: "images/pj-1.svg",
+    projectName: "Professional Art Printing Data",
+    projectNote:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    projectStackUsed: ["HTML", "Bootstrap", "Ruby"],
+  },
+  {
+    id: 4,
+    backImg: "images/project2.svg",
+    projectName: "Data Dashboard",
+    projectNote:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    projectStackUsed: ["HTML", "Bootstrap", "Ruby"],
+  },
+  {
+    id: 5,
+    backImg: "images/pj3.png",
+    projectName: "Website Portfolio ",
+    projectNote:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    projectStackUsed: ["HTML", "Bootstrap", "Ruby"],
+  },
+];
