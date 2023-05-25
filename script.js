dce172dec517412c417606a8bce12f68a811310a
H-Ramalan <talktoramalan@gmail.com>
2023-05-25 07:29:15 -0700

    popup modal functionality

--- script.js
+++ script.js
@@ -21,24 +21,13 @@ for (let i = 0; i < modalList.length; i += 1) {
   modalList[i].addEventListener("click", closeModal);
 }
 
-// Modal Popup
-
-const projectPopup = document.querySelector(".project-popup");
-const btnOpenPopup = document.querySelectorAll(".see-project-appear");
-const btnClosePopup = document.querySelector(".close-popup");
-
-const openPopup = () => {
-  projectPopup.style.display = "block";
+const works = {
+  name: "Multi-Post Stories",
+  description:
+    "A daily selection of privately personalized reads; no accounts or sign-ups required. This has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+  technologies: ["CSS", "HTML", "Bootstrap", "Ruby"],
+  image: "./images/Img Placeholder.svg",
 };
-
-const closePopup = () => {
-  projectPopup.style.display = "none";
-};
-
-for (i = 0; i < btnOpenPopup.length; i += 1) {
-  btnOpenPopup[i].addEventListener("click", openPopup);
-}
-btnClosePopup.addEventListener("click", closePopup);
 
 const projects = [
   {
@@ -90,3 +79,49 @@ const projects = [
     projectStackUsed: ["HTML", "Bootstrap", "Ruby"],
   },
 ];
+
+const projectCards = document.querySelector(".project-cards");
+
+projects.forEach((project, i) => {
+  const projectElement = document.createElement("div"); // div for each project = projectElement
+  projectElement.classList.add("card", `project${i + 1}`);
+
+  // Create HTML structure for each project
+  projectElement.innerHTML = `
+    <div class="project-name">
+      <p class="name">${project.projectName} </p>
+      
+    </div>
+    <p class="project-note">${project.projectNote}</p>
+    <div class = "project-stack">
+      <ul class="project-stack-used">
+        ${project.projectStackUsed
+          .map((tech) => `<li class="tech-used">${tech}</li>`)
+          .join("")}          
+      </ul>
+    </div>
+    <button class="see-project-appear">See project</button>
+  `;
+
+  projectCards.appendChild(projectElement);
+});
+
+// Modal Popup
+
+const projectPopup = document.querySelector(".project-popup");
+const btnOpenPopup = document.querySelectorAll(".see-project-appear");
+const btnClosePopup = document.querySelector(".close-popup");
+
+const openPopup = () => {
+  projectPopup.style.display = "block";
+  project.style.overflow = "auto";
+};
+
+const closePopup = () => {
+  projectPopup.style.display = "none";
+};
+
+for (i = 0; i < btnOpenPopup.length; i += 1) {
+  btnOpenPopup[i].addEventListener("click", openPopup);
+}
+btnClosePopup.addEventListener("click", closePopup);
