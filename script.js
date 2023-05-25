452dde1d64cef274416758f25f75d11745b1db71
H-Ramalan <talktoramalan@gmail.com>
2023-05-25 08:48:46 -0700

    fix linter errors

--- script.js
+++ script.js
@@ -1,82 +1,82 @@
-"use-strict";
+'use-strict';
 
-const modalOpen = document.querySelector(".ham");
-const modalClose = document.querySelector(".nav-close");
-const modal = document.querySelector(".modal-view");
-const modalList = document.querySelectorAll(".list-modal");
+const modalOpen = document.querySelector('.ham');
+const modalClose = document.querySelector('.nav-close');
+const modal = document.querySelector('.modal-view');
+const modalList = document.querySelectorAll('.list-modal');
 
 const openModal = (e) => {
-  modal.style.display = "flex";
+  modal.style.display = 'flex';
   e.preventDefault();
 };
 
 const closeModal = () => {
-  modal.style.display = "none";
+  modal.style.display = 'none';
 };
 
-modalOpen.addEventListener("click", openModal);
-modalClose.addEventListener("click", closeModal);
+modalOpen.addEventListener('click', openModal);
+modalClose.addEventListener('click', closeModal);
 
 for (let i = 0; i < modalList.length; i += 1) {
-  modalList[i].addEventListener("click", closeModal);
+  modalList[i].addEventListener('click', closeModal);
 }
 
 const projects = [
   {
     id: 0,
-    backImg: "images/pj-1.svg",
-    projectName: "Professional Art Printing Data",
+    backImg: 'images/pj-1.svg',
+    projectName: 'Professional Art Printing Data',
     projectNote:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-    projectStackUsed: ["HTML", "Bootstrap", "Ruby"],
+    projectStackUsed: ['HTML', 'Bootstrap', 'Ruby'],
   },
   {
     id: 1,
-    backImg: "images/project2.svg",
-    projectName: "Data Dashboard Healthcare",
+    backImg: 'images/project2.svg',
+    projectName: 'Data Dashboard Healthcare',
     projectNote:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-    projectStackUsed: ["HTML", "Bootstrap", "Ruby"],
+    projectStackUsed: ['HTML', 'Bootstrap', 'Ruby'],
   },
   {
     id: 2,
-    backImg: "images/pj3.png",
-    projectName: "Website Portfolio",
+    backImg: 'images/pj3.png',
+    projectName: 'Website Portfolio',
     projectNote:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-    projectStackUsed: ["HTML", "Bootstrap", "Ruby"],
+    projectStackUsed: ['HTML', 'Bootstrap', 'Ruby'],
   },
   {
     id: 3,
-    backImg: "images/pj-1.svg",
-    projectName: "Professional Art Printing Data",
+    backImg: 'images/pj-1.svg',
+    projectName: 'Professional Art Printing Data',
     projectNote:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-    projectStackUsed: ["HTML", "Bootstrap", "Ruby"],
+    projectStackUsed: ['HTML', 'Bootstrap', 'Ruby'],
   },
   {
     id: 4,
-    backImg: "images/project2.svg",
-    projectName: "Data Dashboard",
+    backImg: 'images/project2.svg',
+    projectName: 'Data Dashboard',
     projectNote:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-    projectStackUsed: ["HTML", "Bootstrap", "Ruby"],
+    projectStackUsed: ['HTML', 'Bootstrap', 'Ruby'],
   },
   {
     id: 5,
-    backImg: "images/pj3.png",
-    projectName: "Website Portfolio ",
+    backImg: 'images/pj3.png',
+    projectName: 'Website Portfolio ',
     projectNote:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-    projectStackUsed: ["HTML", "Bootstrap", "Ruby"],
+    projectStackUsed: ['HTML', 'Bootstrap', 'Ruby'],
   },
 ];
 
-const projectCards = document.querySelector(".project-cards");
+const projectCards = document.querySelector('.project-cards');
 
 projects.forEach((project, i) => {
-  const projectElement = document.createElement("div"); // div for each project = projectElement
-  projectElement.classList.add("card", `project${i + 1}`);
+  const projectElement = document.createElement('div'); // div for each project = projectElement
+  projectElement.classList.add('card', `project${i + 1}`);
 
   // Create HTML structure for each project
   projectElement.innerHTML = `
@@ -88,8 +88,8 @@ projects.forEach((project, i) => {
     <div class = "project-stack">
       <ul class="project-stack-used">
         ${project.projectStackUsed
-          .map((tech) => `<li class="tech-used">${tech}</li>`)
-          .join("")}          
+    .map((tech) => `<li class="tech-used">${tech}</li>`)
+    .join('')}          
       </ul>
     </div>
     <button class="see-project-appear">See project</button>
@@ -100,27 +100,28 @@ projects.forEach((project, i) => {
 
 // Modal Popup
 
-const projectPopup = document.querySelector(".project-popup");
+const projectPopup = document.querySelector('.project-popup');
 
-const backdropPopup = document.querySelector(".backdrop-popup");
-const btnOpenPopup = document.querySelectorAll(".see-project-appear");
-const btnClosePopup = document.querySelector(".close-popup");
-const recentWorksPopup = document.querySelector(".see-project-btn");
+const backdropPopup = document.querySelector('.backdrop-popup');
+const btnOpenPopup = document.querySelectorAll('.see-project-appear');
+const btnClosePopup = document.querySelector('.close-popup');
+const recentWorksPopup = document.querySelector('.see-project-btn');
 
 const openPopup = () => {
-  projectPopup.style.display = "block";
-  projectPopup.style.overflow = "auto";
-  backdropPopup.style.display = "block";
-  backdropPopup.style.overflow = "auto";
+  projectPopup.style.display = 'block';
+  projectPopup.style.overflow = 'auto';
+  backdropPopup.style.display = 'block';
+  backdropPopup.style.overflow = 'auto';
 };
 
 const closePopup = () => {
-  projectPopup.style.display = "none";
-  backdropPopup.style.display = "none";
+  projectPopup.style.display = 'none';
+  backdropPopup.style.display = 'none';
 };
 
-for (i = 0; i < btnOpenPopup.length; i += 1) {
-  btnOpenPopup[i].addEventListener("click", openPopup);
+for (let i = 0; i < btnOpenPopup.length; i += 1) {
+  btnOpenPopup[i].addEventListener('click', openPopup);
 }
-btnClosePopup.addEventListener("click", closePopup);
-recentWorksPopup.addEventListener("click", openPopup);
+
+btnClosePopup.addEventListener('click', closePopup);
+recentWorksPopup.addEventListener('click', openPopup);
